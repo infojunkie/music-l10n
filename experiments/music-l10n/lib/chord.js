eef332427b5c5edec3d7938cd6b79e77d983a674
infojunkie <karim.ratib@gmail.com>
2016-08-19 01:18:44 -0700

    Scale names, missing modes

--- experiments/music-l10n/lib/chord.js
+++ experiments/music-l10n/lib/chord.js
@@ -1,5 +1,4 @@
-// chord generation
-// Given a scale, return all its chords up to the required depth
+// chord functions
 
 import _ from 'lodash';
 
@@ -39,76 +38,6 @@ Interval.prototype.name = function() {
   return Interval.INTERVALS_TO_NAMES[this.delta];
 }
 
-/* Scale */
-export function Scale(intervals, tones, accidental_sign) {
-  this.intervals = intervals;
-  this.tones = tones;
-  this.accidental_sign = accidental_sign;
-}
-Scale.fromTones = function(tones, accidental_sign) {
-  return new Scale(
-    tones.concat(tones[0] + Scale.TUNING_TONES).reduce( (intervals, tone, index, tones) => {
-      return index ? intervals.concat(tone - tones[index - 1]) : intervals;
-    }, []),
-    tones,
-    accidental_sign
-  );
-}
-Scale.fromIntervals = function(intervals, start, accidental_sign) {
-  return new Scale(
-    intervals,
-    intervals.reduce( (tones, interval, index) => {
-      return tones.concat(tones[index] + interval);
-    }, [start]).slice(0,-1),
-    accidental_sign
-  );
-}
-Scale.TUNING_TONES = 12;
-Scale.TUNING_NAMES_TO_TONES = {
-  'C': 0,
-  'D': 2,
-  'E': 4,
-  'F': 5,
-  'G': 7,
-  'A': 9,
-  'B': 11
-};
-Scale.TUNING_TONES_TO_NAMES = {
-  0: 'C',
-  2: 'D',
-  4: 'E',
-  5: 'F',
-  7: 'G',
-  9: 'A',
-  11: 'B'
-};
-Scale.prototype.toneAt = function(degree) {
-  // Find the base tone and then raise it to the given octave
-  // Remember the degree is [-inf, +inf]
-  // To understand a / b | 0, see http://stackoverflow.com/questions/7487977/using-bitwise-or-0-to-floor-a-number
-  return this.tones[ degree % this.tones.length ] +
-         (degree / this.tones.length | 0) * Scale.TUNING_TONES;
-}
-Scale.prototype.noteAt = function(degree) {
-  var tone = this.toneAt(degree);
-  var base = tone % Scale.TUNING_TONES;
-  var name = Scale.TUNING_TONES_TO_NAMES[base];
-  if (!name) {
-    // We have an intermediate tone: get the accidental
-    var adjusted_base = (base - this.accidental_sign) % Scale.TUNING_TONES;
-    var accidental = this.accidental_sign > 0 ? '#' : 'b';
-    name = Scale.TUNING_TONES_TO_NAMES[adjusted_base] + accidental;
-  }
-  return name;
-}
-Scale.prototype.spell = function() {
-  var scale = this;
-  return this.tones.map( (tone, degree) => {
-    return scale.noteAt(degree);
-  });
-}
-
-/* Chord */
 export function Chord(scale, degrees) {
   this.scale = scale;
   this.degrees = degrees;
--- experiments/music-l10n/test/chord_test.js
+++ experiments/music-l10n/test/chord_test.js
@@ -1,28 +1,12 @@
 import { deepEqual } from 'assert';
 
 import '../lib';
-import { /*Interval, */Scale, Chord } from '../lib/chord.js';
+import { Scale } from '../lib/scale.js';
+import { Chord } from '../lib/chord.js';
 
 describe('Chord', () => {
   const D = Scale.fromTones([2, 4, 6, 7, 9, 11, 13], +1 /* sharp +1 or flat -1 */);
-  const Bb = Scale.fromTones([10, 12, 14, 15, 17, 19, 21], -1);
-  const Am = Scale.fromIntervals([2,1,2,2,1,2,2], 9);
   const Dchords = Chord.chordsFromScale(D, 4 /* notes per chord */);
-
-  it('generates correct intervals from tones', () => {
-    deepEqual(D.intervals, [2,2,1,2,2,2,1]);
-    deepEqual(Bb.intervals, [2,2,1,2,2,2,1]);
-  });
-
-  it('generates correct tones from intervals', () => {
-    deepEqual(Am.tones, [9, 11, 12, 14, 16, 17, 19], +1);
-  });
-
-  it('generates correct note spellings', () => {
-    deepEqual(D.spell(), ['D', 'E', 'F#', 'G', 'A', 'B', 'C#']);
-    deepEqual(Bb.spell(), ['Bb', 'C', 'D', 'Eb', 'F', 'G', 'A']);
-    deepEqual(Am.spell(), ['A', 'B', 'C', 'D', 'E', 'F', 'G']);
-  });
 
   it('generates correct chords from a scale', () => {
     deepEqual(Dchords.map( (chord) => { return chord.degrees; }),
